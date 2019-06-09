2ce963f34b5b3c27719605e835b8babf2c013fea
Scott Lindauer <smlindau@ncsu.edu>
2019-06-08 21:27:36 -0400

    Update generateMosaicImage.m

--- generateMosaicImage.m
+++ generateMosaicImage.m
@@ -1,30 +1,41 @@
 function generateMosaicImage()
+
 %% Main Image Choice
 disp('Choose main mosaic image...');
 [mainImageName,mainImagePath] = uigetfile({'*.png;*.jpg'});
+% Normalize the image from 0 to 1 and change type to double
 mainImage = getNormalized(double(imread(fullfile(mainImagePath,...
     mainImageName))));
 [mainRows,mainCols,mainColors] = size(mainImage);
 pixelArea = mainRows*mainCols;
+
 %% Mosaic Image Directory Choice
 disp('Choose directory of mosaic images...');
 mosaicImagesPath = uigetdir;
 imageJpgDir = dir(fullfile(mosaicImagesPath,'*.jpg'));
 imagePngDir = dir(fullfile(mosaicImagesPath,'*.png'));
+% Determine the number of images in the directory
 numMosaicImages = size(imageJpgDir,1)+size(imagePngDir,1);
+% Set the size of each small image based on the number of images and the size of the original image
 mosaicSquareSize = floor(sqrt(pixelArea/numMosaicImages));
+% Initialize the image stack in RGB
 imageStack = zeros(mosaicSquareSize,mosaicSquareSize,3*numMosaicImages);
 imageStackRGB = zeros(3,numMosaicImages);
+
 %% Create Mosaic Grid
+% Grid the initial image to fit all of the smaller images
 wholeBlockRows = floor(mainRows / mosaicSquareSize);
 blockVectorR = mosaicSquareSize * ones(1, wholeBlockRows);
 wholeBlockCols = floor(mainCols / mosaicSquareSize);
 blockVectorC = mosaicSquareSize * ones(1, wholeBlockCols);
+% Crop main image to remove any excess space not covered by smaller images
 cropMainImage = mainImage(1:end-mod(mainRows,mosaicSquareSize),...
     1:end-mod(mainCols,mosaicSquareSize),:);
 cellArray = mat2cell(cropMainImage,blockVectorR,blockVectorC,mainColors);
 cellArrayRGB = cellfun(@meanRGB,cellArray,'UniformOutPut',0);
+
 %% Resize Mosaic Images
+% Each mosaic image needs to be the same size, so this loops through all images to resize them
 for i = 1:numMosaicImages
     if i <= size(imageJpgDir,1)
         oversizedImage = getNormalized(double(imread(fullfile(...
@@ -93,7 +104,9 @@ for i = 1:numMosaicImages
     end
     disp(strcat(num2str(i),'/',num2str(numMosaicImages)))
 end
+
 %% Find Closest RGB Cell
+% For each mosaic image, compare their RGB values and assign a location
 [cellRow,cellCol] = size(cellArrayRGB);
 mosaicImageUse(numMosaicImages) = struct('Index',[],'InUse',[],'Cell',[]);
 for i = 1:numMosaicImages
@@ -110,6 +123,7 @@ for i = 1:cellRow
         mosaicImageUse(distInd(imageInd)).Cell = [i,j];
     end
 end
+
 %% Assign Transparent Imaging
 mosaicCell = cell(size(cellArray));
 overlay = .4;
@@ -124,6 +138,7 @@ for i = 1:numMosaicImages
     end
 end
 imshow(cell2mat(mosaicCell))
+
 %% Anonymous Functions
     function B = meanRGB(A)
         B(:,1) = mean(mean(A(:,:,1)));
